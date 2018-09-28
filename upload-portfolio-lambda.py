e8a56a6be2d5a591d3ef043b47b2346648ec489c
Alex Paun <alexandrupn@gmail.com>
2018-09-28 15:04:16 +0300

    Added Mimecast library

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -1,13 +1,20 @@
 import boto3
-s3 = boto3.resource ('s3')
+from botocore.client import Config
+import StringIO
+import zipfile
+import mimetypes
+
+s3 = boto3.resource ('s3', config=Config(signature_version='s3v4'))
+
 portfolio_bucket = s3.Bucket('portfolio.gesq.net')
 build_bucket = s3.Bucket('portfoliobuild.gesq.net')
-import StringIO
+
 portfolio_zip = StringIO.StringIO()
 build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
-import zipfile
+
 with zipfile.ZipFile(portfolio_zip) as myzip:
     for nm in myzip.namelist():
         obj = myzip.open(nm)
-        portfolio_bucket.upload_fileobj(obj, nm)
+        portfolio_bucket.upload_fileobj(obj, nm,
+          ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
         portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
